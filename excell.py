4fa98fd89ed5fc0f314ee33a15656a9ec5939ffa
hpeter19 <njoshpitar@gmail.com>
2025-03-05 11:50:00 +0300

    printing values of  a cell

--- excell.py
+++ excell.py
@@ -4,6 +4,8 @@ wb = xl.load_workbook('transactions.xlsx')
 sheet= wb['Sheet1']
 cell = sheet ['b2']
 
-print(sheet.max_row)
-print(sheet.max_column)
-#checking the number of rows and columns on a spredsheet
+
+for raw in range(1,sheet.max_row + 1):
+    cell=sheet.cell(raw,3)
+    print(cell.value)
+#printing values of each cell
