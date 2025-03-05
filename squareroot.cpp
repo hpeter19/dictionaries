cbd38a3c583e8877214bd5abecf3024f7af3f7cf
hpeter19 <njoshpitar@gmail.com>
2025-03-05 10:31:03 +0300

    A program to calculate the squareroot of a number input by the user

---
+++ squareroot.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+ {
+float Number,squareroot;
+cout<<"Enter a Number: " "\n";
+cin>>Number
+squareroot = sqrt(Number);
+cout<<"The square root of Number is: " "\n";
+cout<< squareroot;
+return 0;
+
+
+
+
+
+
+
+}
