060f93c80720a4e174bc07bcb44add61a98428a5
hpeter19 <njoshpitar@gmail.com>
2025-03-06 19:47:33 +0300

    the recursion

--- recursionc++.cpp
+++ recursionc++.cpp
@@ -1,4 +1,8 @@
 #progogram to demonstrate recursion in c++
+#The factorial function is a recursive function that calculates the factorial of a number.
+#The base case is when n == 0. According to the definition of factorial, 0! = 1, so when n is 0, the function returns 1.
+#For other values of n, the function calls itself recursively: factorial(n) = n * factorial(n - 1).
+#The main function asks the user for an integer and calculates the factorial using recursion.
 
 #include<iostream>
 using namespace std;
@@ -25,6 +29,7 @@ int main() {
     int result = factorial(num);
 
     cout << "The factorial of " << num << " is: " << result << endl;
-
+#
     return 0;
 }
+# 
