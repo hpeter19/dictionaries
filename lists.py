34e5d50f5f91e0872b5f03632f65287a6e95ef67
hpeter19 <njoshpitar@gmail.com>
2025-03-07 10:15:05 +0300

    items in a list

--- lists.py
+++ lists.py
@@ -1,10 +1,10 @@
-#2D list
-groceries = [["Oranges","Mangoes","Oranges"],
- ["Sukuma","Spinach","potatoes"],
-["Chicken","Pork","Beef"]]
+    #2D list
+    groceries = [["Oranges","Mangoes","Oranges"],
+    ["Sukuma","Spinach","potatoes"],
+    ["Chicken","Pork","Beef"]]
 
-#printing every item on our loop using nested for loops
-for collection in groceries:
-    for food in collection:
-        print(food, end=" ")#these will print each item in a vertical manner
-      print()  
+    #printing every item on  our loop using nested for loops
+    for collection in groceries:
+        for food in collection:
+            print(food, end=" ")#these will print each item in a vertical manner
+        print()  
