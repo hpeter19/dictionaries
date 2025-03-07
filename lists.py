a1ebefaef3356b61693eeccad7e3259a480ffe59
hpeter19 <njoshpitar@gmail.com>
2025-03-07 09:55:09 +0300

    lists in python

---
+++ lists.py
@@ -0,0 +1,7 @@
+fruits = ["Oranges","Mangoes","Oranges"]
+vegetables = ["Sukuma","Spinach","potatoes"]
+meat =["Chicken","Pork","Beef"]
+
+groceries =[fruits,vegetables,meat]
+#printing item on row 3 index 2
+print (groceries[2][2])
