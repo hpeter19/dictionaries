fa49884a4fd17a0440617ce623b29417569ed534
hpeter19 <njoshpitar@gmail.com>
2025-03-10 11:14:45 +0300

    args and kwargs functions

---
+++ argskwargs.py
@@ -0,0 +1,7 @@
+# args -allows you to pass multiple non -key arguments
+# kwargs - allows you to pass multiple keyword -arguments
+# *unpacking character
+
+def add(a,b):
+    return a+b
+print(add(1,3))
