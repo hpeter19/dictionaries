685d27cd362971a7963006aaa190e23381af9bd9
hpeter19 <njoshpitar@gmail.com>
2025-03-10 11:50:53 +0300

    using item method to return pairs as statements

--- argskwargs.py
+++ argskwargs.py
@@ -1,12 +1,12 @@
 # args -allows you to pass multiple non -key arguments
 # kwargs - allows you to pass multiple keyword -arguments
 # *unpacking character
-# printing keys  using kwargs
+# item method returns key value pairs
 
 
 def address_name(**kwargs):
-    for key in kwargs.keys():
-        print(key)
+    for key,values in kwargs.items():
+        print(f"{key},{values}")
 address_name(street="23 Molo",
              city="Nairobi",
              state="Nakuru",
