314ebb47e7c4a0a552ebad4e9dd5b56dae5a1f37
hpeter19 <njoshpitar@gmail.com>
2025-03-12 19:06:17 +0300

    getting the item from a set

---
+++ setsstring.py
@@ -0,0 +1,7 @@
+students ={"Mark","Alex","Miguel","Mark"}
+
+student =input("Enter the name of the student")
+if student in students:
+    print(f"{student} is a student")
+else:
+    print(f"{student} was not found")    
