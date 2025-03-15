5672b64f551450ee84713f181c2602dedb1ff73f
hpeter19 <njoshpitar@gmail.com>
2025-03-15 12:07:37 +0300

    using pytz in UTC

--- datetime.py
+++ datetime.py
@@ -1,13 +1,13 @@
 import datetime
+import pytz
 from datetime import timedelta
 
-dt_today =datetime.datetime.today()
-dt_now=datetime.datetime.now()
-dt_utcnow =datetime.datetime.utcnow()
-#today returns current local time with today with no timezone
-#now allows us to pass a timezone
-#utcnow gives urrent uct time
-print(dt_today)
+dt=datetime.datetime(2025,3,15,12,1,26, tzinfo=pytz.UTC)
+print(dt)
+dt_now=datetime.datetime.now(pytz.UTC)
 print(dt_now)
+dt_utcnow =datetime.datetime.utcnow().replace( tzinfo=pytz.UTC)
 print(dt_utcnow)
 
+
+#using pytz to print UTC time
