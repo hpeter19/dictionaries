66abde93e5ad56f17b6189bcb3f2df8cdd288ae8
hpeter19 <njoshpitar@gmail.com>
2025-03-16 23:08:02 +0300

    printing in descending order

--- sorting.py
+++ sorting.py
@@ -1,8 +1,8 @@
 li = [1,5,3,7,10,4,23]
 
-l_sorted =sorted(li)
+l_sorted =sorted(li,reverse=True)
 print('sorted variables:\t',l_sorted)
 li.sort()
 print('sorted variables:\t', li)
-#sorting in ascending order
+#sorting in Descending order
 #printing original sorted list using.sort() method 
