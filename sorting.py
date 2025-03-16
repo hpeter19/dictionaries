0aadada2418c07cd770e80baf0419391738d9a47
hpeter19 <njoshpitar@gmail.com>
2025-03-16 23:05:24 +0300

    printing a sorted list in an array

--- sorting.py
+++ sorting.py
@@ -2,4 +2,7 @@ li = [1,5,3,7,10,4,23]
 
 l_sorted =sorted(li)
 print('sorted variables:\t',l_sorted)
+li.sort()
+print('sorted variables:\t', li)
 #sorting in ascending order
+#printing original sorted list using.sort() method 
