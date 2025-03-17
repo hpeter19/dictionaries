6ed80121c9c2d0768882faded3ca08187128ed26
hpeter19 <njoshpitar@gmail.com>
2025-03-17 15:25:38 +0300

    capitilizing first letter of a string

--- lenthstrings.py
+++ lenthstrings.py
@@ -2,7 +2,7 @@
 name = input("Enter your Names:")
 
 #result=len(name)
-#.find returns the first occurence of a given character. The position
-#prints the last occurence.Incase the character appears twice"
-result =name.rfind(" ")
-print(result)
+#capitilizing the first letter of a string
+name = name.capitalize()
+
+print(name)
