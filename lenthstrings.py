bc50594664edd4875d2d6e2f051db6c9c51e6a3a
hpeter19 <njoshpitar@gmail.com>
2025-03-17 15:28:37 +0300

    lower case

--- lenthstrings.py
+++ lenthstrings.py
@@ -2,7 +2,7 @@
 name = input("Enter your Names:")
 
 #result=len(name)
-#capitilizing the whole string
-name = name.upper()
+#capitilizing the whole string in undercase
+name = name.lower()
 
 print(name)
