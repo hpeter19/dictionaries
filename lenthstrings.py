23aa395c3cb62f1541ac3849899519098102986a
hpeter19 <njoshpitar@gmail.com>
2025-03-17 15:18:26 +0300

    last occurence

--- lenthstrings.py
+++ lenthstrings.py
@@ -3,5 +3,6 @@ name = input("Enter your Names:")
 
 #result=len(name)
 #.find returns the first occurence of a given character. The position
-result =name.find(" ")
+#prints the last occurence.Incase the character appears twice"
+result =name.rfind(" ")
 print(result)
