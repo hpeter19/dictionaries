456c2f34c198c3bc1b9617166f7d695d4e7fd448
hpeter19 <njoshpitar@gmail.com>
2025-03-18 09:04:32 +0300

    last elements in astring

--- indexing.py
+++ indexing.py
@@ -1,5 +1,6 @@
 credit_number=["1234-1415-1516-1762"]
 ###printing the index of a string
 #print(credit_number[0])
-#printing the first four digits
-print(credit_number[0:4])
+#print(credit_number[0:4])
+#prints the last 12 digits in a string
+credit_number[5:] 
