fe411ac0e825358d5fb7f4b282e3e06949430f64
hpeter19 <njoshpitar@gmail.com>
2025-03-22 10:27:09 +0300

    item method for key pairs in kwargs

--- defaultarguments.py
+++ defaultarguments.py
@@ -1,9 +1,10 @@
 #arbitrary Argument
 #Kwargs
 #values in an kwargs for key items
+#item inbuild function returns key pairs
 def address_info(**kwargs):
-    for key in kwargs.keys():
-        print(key)
+    for key,value in kwargs.items():
+        print(f"{key}: {value}")
 
 address_info(street="Mungoni",
              city="Chuka",
