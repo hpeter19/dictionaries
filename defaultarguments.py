869a0a0aa6c891c7c10eb2b8f9621e7f7d4ea7ef
hpeter19 <njoshpitar@gmail.com>
2025-03-22 09:39:26 +0300

    keyword arguments functions

--- defaultarguments.py
+++ defaultarguments.py
@@ -1,5 +1,6 @@
-#Setting default values for discount and tax
-def net_price(list_price,discount=0,tax=0.05):
-    return list_price *(1-discount)*(1+tax)
 
-print(net_price(1000,0,0.05))
+#Keyword Arguments
+def hello(greeting,title,first,last):
+    print(f"{greeting} {title} {first} {last}")
+
+hello("GOOD MORNING", title="Dr.", first="Margarete", last="Salama")
