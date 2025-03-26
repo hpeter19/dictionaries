451dcdc0aaa079bdd4ad6dc18ef5261b30228237
hpeter19 <njoshpitar@gmail.com>
2025-03-26 15:44:42 +0300

    creating pairs of two

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,6 +1,8 @@
 nums =[1,2,3,4,5,6,7,8,9]
 
-my_list= list(filter( lambda num: num%2 == 0,nums))
-
+my_list= []
+for letter in "abcd":
+    for num in range( 4):
+        my_list.append((letter,num))
 print(my_list)
-#using lambda to find modulus
+#creating pairs of two
