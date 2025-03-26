57151b80a0bfa1610b2e8a5aed2fefb8dacfa0f4
hpeter19 <njoshpitar@gmail.com>
2025-03-26 16:10:17 +0300

    set compression

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,10 +1,5 @@
 nums =[1,2,3,4,5,6,7,8,9,10,11,1,2,2]
 
-my_set=set()
-
-for set in nums:
-    my_set.add(set)
-
+my_set= {num for num in nums}
 print(my_set)
-#set prints only unique values
-
+#set compression 
