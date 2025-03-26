4dc5b5faca116e77044fbf6f4e8484f21058a423
hpeter19 <njoshpitar@gmail.com>
2025-03-26 15:33:57 +0300

    modulus using compression method

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,8 +1,6 @@
 nums =[1,2,3,4,5,6,7,8,9]
 
-my_list=[]
-for num in nums:
-    if num%2 == 0:
-        my_list.append(num)
+my_list=[num for num in nums if num%2 == 0]
+
 print(my_list)
-#modulus
+#modulus using compression method
