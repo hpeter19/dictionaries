67e758b5e25da55fce9dff448832517b643f26b5
hpeter19 <njoshpitar@gmail.com>
2025-03-26 16:04:44 +0300

    setting up conditions

--- listcomprehension.py
+++ listcomprehension.py
@@ -3,7 +3,7 @@ nums =[1,2,3,4,5,6,7,8,9]
 names=['Martin','Mark','Folix','Ann','Mary']
 heroes=['Batman','Spiderman','Hulk','Deadpool','Batman' ]
 
-my_dict= {name:hero for name ,hero in zip(names,heroes) }
+my_dict= {name:hero for name ,hero in zip(names,heroes) if name !='Mark' }
 
 print(my_dict)
 #using compression to form a list from two lists of data
