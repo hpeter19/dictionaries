9e4533da4c0db300b62eb5c0fc39edde4397c555
hpeter19 <njoshpitar@gmail.com>
2025-03-26 15:26:53 +0300

    lamda for iterating

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,7 +1,6 @@
 nums =[1,2,3,4,5,6,7,8,9]
 
 
-my_list=[num*num for num in nums]
-
-print(my_list)
-#printing the square using compressed loop
+my_list = list(map(lambda n: n*n,nums))
+print (my_list)
+#printing the square using lambda.Convert the map object into a list and iterate over it.
