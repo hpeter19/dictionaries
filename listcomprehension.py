40eb44c9e58dcb6b3e09ceef35fc461831bbf97a
hpeter19 <njoshpitar@gmail.com>
2025-03-26 16:01:48 +0300

    compression of two list

--- listcomprehension.py
+++ listcomprehension.py
@@ -3,8 +3,7 @@ nums =[1,2,3,4,5,6,7,8,9]
 names=['Martin','Mark','Folix','Ann','Mary']
 heroes=['Batman','Spiderman','Hulk','Deadpool','Batman' ]
 
-my_dict= {}
-for names,heroes in  zip(names,heroes):
-    my_dict[names]= heroes
+my_dict= {name:hero for name ,hero in zip(names,heroes) }
+
 print(my_dict)
-#creating a pair of values in a dictionary using the zip method to generate alist
+#using compression to form a list from two lists of data
