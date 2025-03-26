c83190f9b8c1aaa75d683937c555812f43e23171
hpeter19 <njoshpitar@gmail.com>
2025-03-26 15:47:35 +0300

    compression to print out pairs

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,8 +1,5 @@
 nums =[1,2,3,4,5,6,7,8,9]
 
-my_list= []
-for letter in "abcd":
-    for num in range( 4):
-        my_list.append((letter,num))
+my_list= [(letter,num) for letter in "abcd" for num in range(4)]
 print(my_list)
-#creating pairs of two
+#using compression to print out pairs
