cf053d60f68911e7b44b9fbd15d13cf7d29977b5
hpeter19 <njoshpitar@gmail.com>
2025-03-27 10:51:40 +0300

    printing names and grades in a dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,10 @@
-my_dictionary = {"A":2,"B":3,"C":4}
-for key,value in my_dictionary.items():
-    print(key,value)
+grades = {"Sandy":"A","Mark":"B","Mary":"A","Gilbert":"C"}
+
+student=input("Enter the name of the student: ")
+
+if student in grades:
+    print(f"{student}'s grade is {grades[student]}")
+else:
+    print(f"{student} was not found")
+
+##printing name and grades in a dictionary
