93a0e364f83d38c31a747909df85cc94466c9c52
peter njjosh <njoshpitar@gmail.com>
2025-06-20 22:58:30 +0300

    Update email.py

--- email.py
+++ email.py
@@ -4,3 +4,9 @@ if "@" in email and ".":
     print("valid email")
 else:
     print("invalid email")    
+email ="njoro@gmail.com"
+
+if "@" in email and ".":
+    print("valid email")
+else:
+    print("invalid email")
